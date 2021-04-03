2ca2be765216fd5b24f4a7b9e2710d133cc39c23
Daniel Lima Oliveira <daniel102510@hotmail.com>
2021-04-03 01:38:52 -0300

    Example packages dependencys

--- teste.py
+++ teste.py
@@ -1,23 +1,19 @@
 import sys
 import time
 
-sys.path.append('../')
+MIDSI_PATH = '/home/daniel/Documentos/midsi'
+
+sys.path.append(MIDSI_PATH)
 from wsmlparser.parser import *
+
 reasoner = Reasoner()
-print("== Teste.py")
-#reasoner.load('wsmlcodes/ont1-ShipmentOntology/ShipmentOntology.wsml')
+
+# Curl dependencys
+package = 'curl'
+
 start = time.time()
-reasoner.load('./Repository/Packages/teste.wsml')
+reasoner.load('./Repository/Packages/'+package+'.wsml')
 finish = time.time()
 countTime = (finish-start)*1000
-print()
-#print(str(round(countTime, 2)) + ' ms')
-#print(str(round(countTime, 2) / 1000) + ' s')
-#print(str(round(countTime, 2) / 1000 / 60) + ' min')
-#print()
-#print(reasoner.executeProlog('depends(X,Y,Z)'))
-#print(reasoner.execute('packageDependences("code",?dependency)'))
-#print(reasoner.execute('packageUri("code",?repo,?package)'))
-#print(reasoner.execute("?x memberOf Package"))
-print(reasoner.executeProlog('concept(X)'))
-#print(reasoner.executeProlog('relationType(X,Y)'))
+
+print(reasoner.execute('depends(?x,?y,?z)'))
